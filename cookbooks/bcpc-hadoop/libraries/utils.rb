a5ef3eacc5007c954cd183a622dcca84f30e9569
ronny-macmaster <ronnymacmaster@gmail.com>
2018-06-01 11:08:57 -0400

    fix floating host management vip for oozie url

--- cookbooks/bcpc-hadoop/libraries/utils.rb
+++ cookbooks/bcpc-hadoop/libraries/utils.rb
@@ -271,7 +271,7 @@ def set_hosts
   if oozie_hosts.length > 1
     # high-availability
     node.default[:bcpc][:hadoop][:oozie_url] = 
-      "http://#{node[:bcpc][:management][:viphost]}:" +
+      "http://#{float_host(node[:bcpc][:management][:viphost])}:" +
         "#{node[:bcpc][:ha_oozie][:port]}/oozie"
   elsif oozie_hosts.length == 1
     # single oozie host
